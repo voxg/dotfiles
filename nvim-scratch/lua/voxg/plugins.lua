08fc9052ba6a2572ff42446440ff999cfc99ed76
Greg Lyons <greg@voxg.us>
2022-12-20 22:37:06 -0600

    use lsp-zero for more better LSP kickstart

--- nvim-scratch/lua/voxg/plugins.lua
+++ nvim-scratch/lua/voxg/plugins.lua
@@ -31,10 +31,32 @@ return require('packer').startup(function(use)
 
   use { 'windwp/nvim-autopairs' }
 
+--  use {
+--      'williamboman/mason.nvim',
+--      'williamboman/mason-lspconfig.nvim',
+--      'neovim/nvim-lspconfig',
+--  }
+
   use {
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'neovim/nvim-lspconfig',
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+          -- LSP
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          {'rafamadriz/friendly-snippets'},
+      }
   }
 
   use { 'lewis6991/gitsigns.nvim' }
