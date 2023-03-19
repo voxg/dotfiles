a6019b4e5c4e2912439604a4af354ceb223702fa
Greg Lyons <GLYONS@hubgroup.com>
2023-03-18 19:47:04 -0500

    Fix mappings for H and L even more better

--- nvim-astro/init.lua
+++ nvim-astro/init.lua
@@ -108,8 +108,18 @@ local config = {
             -- second key is the left side of the map
             ["<leader>n"] = { desc = "Neorg" },
             ["<leader>ni"] = { "<cmd>Neorg index<cr>", desc = "Neorg index"},
-            ["H"] = { "<cmd>bprevious<cr>", desc = "Previous buffer"},
-            ["L"] = { "<cmd>bnext<cr>", desc = "Next buffer"},
+            ["H"] = {
+                function ()
+                    require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+                end,
+                desc = "Previous buffer"
+            },
+            ["L"] = {
+                function ()
+                    require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+                end,
+                desc = "Next buffer"
+            },
         },
     },
 
