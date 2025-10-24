030ae5c09e4ea4a480722d0ccc95f90b433e0358
Greg Lyons <greg@voxg.us>
2025-10-23 21:42:13 -0500

    Add systemd-supervised ssh-agent support to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+if test -z "$SSH_AUTH_SOCK"
+    set -f socket_path "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    test -e $socket_path && set -x SSH_AUTH_SOCK $socket_path
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
